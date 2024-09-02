bbc8003744fda84e1c7046a08275e73f060cb989
Daniel Adekoya <adekoyadaniel53@gmail.com>
2024-09-02 09:52:16 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const session = require("express-session");
 const mongoConnect = require("connect-mongo");
+const connectFlash = require("connect-flash");
+
 
 const app = express();
 const router = require("./router");
@@ -18,7 +20,21 @@ app.use(sessionOptions);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
+  var logged = false;
+
+  /*In your function*/
+  if (!logged) {
+    console.log(req.headers)
+    logged = true;
+  }
+  next();
+});
 app.use(express.static("public"));
 app.set("views", "pages");
 app.set("view engine", "ejs");
--- db.js
+++ db.js
@@ -1,13 +1,21 @@
 const { MongoClient } = require("mongodb");
+const ip = require("ip");
+const chalk = require('cli-color');
+
 const dotenv = require("dotenv");
 dotenv.config();
 const { PORT, CONNECTION_STRING } = process.env;
+const local = 'http://' + 'localhost' + ':' + PORT
+const newtWork = 'http://' + ip.address() + ':' + PORT
 const connectDb = async () => {
   const db = new MongoClient(CONNECTION_STRING);
   await db.connect();
   module.exports = db;
   const app = require("./app");
   app.listen(PORT);
-  console.log("Listen on http://localhost:" + PORT);
+  console.group()
+  console.log('> Listen on Local ' + chalk.blue(local));
+  console.log('> Listen on Network ' + chalk.blue(newtWork));
+
 };
 connectDb();
